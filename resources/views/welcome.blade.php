4dd14155d0addd009db4b2c6885864dd05ce8f6c
rixjoseph <mca517@rajagiri.edu>
2021-03-16 23:25:04 -0700

    Student

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col col-12 col-sm-3 col-md-3 col-md-6 col-lg-6 col-xl-12 col-xxl-12">
+        <center>
+        <table class="table table-borderless">
+        <tr>
+            <td>Name</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>ROLL NO</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>COLLEGE NAME</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>ADMISSION NO</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><button class="btn btn-warning">SUBMIT</button></td>
+        </tr>
+        </table>
+        </center>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
